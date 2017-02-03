d1559d8bbb13000f44a8a72282620a094a25b5fe
luumee <alexluulim@gmail.com>
2017-02-03 15:05:02 +0000

    updated UDPSender.py to work with piface

--- UDPSender.py
+++ UDPSender.py
@@ -1,14 +1,10 @@
 # Source: https://pymotw.com/2/socket/udp.html
 
 import socket, sys, time
-import RPi.GPIO as io
-import sys
+import pifacedigitalio as pfio
 
+pfio.init();
 
-io.setmode(io.BCM)
-io.setup(23,io.IN,pull_up_down=io.PUD_UP)
-io.setup(24,io.IN,pull_up_down=io.PUD_UP)
-io.setup(25,io.IN,pull_up_down=io.PUD_UP)
 
 
 host = sys.argv[1]
@@ -17,35 +13,32 @@ textport = sys.argv[2]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
+flag = False;
 
 while 1:
-    b1=io.input(23)
-    b2=io.input(24)
-    b3=io.input(25)
-    b4=io.input(26)
+    b1=pfio.digital_read(0)
+    b2=pfio.digital_read(1)
+    b3=pfio.digital_read(2)
+    b4=pfio.digital_read(3)
 
     data = bytearray([0]*100)
 
-    if b1!=1:
-        data[7] = 1
-        #print ("Enter data to transmit: ENTER to quit")
-        #data = sys.stdin.readline().strip()
-        #if not len(data):
-        #break       
-    if b2!=1:
-        data[6] = 1
-    if b3!=1:
-        data[5] = 1
-        data[7] = 1
-    if b4!=1:
-        data[4] = 1
+    flag = False
+    if b1==1:
+        data[0] = '2'
+	flag = True
+    if b2==1:
+        data[0] = '3'
+	flag = True
+    if b3==1:
+        data[0] = '4'
+	flag = True
+    if b4==1:
+	data[0] = '5'
+	flag = True
     
-    #print ("Enter data to transmit: ENTER to quit")
-    #data = sys.stdin.readline().strip()
-    #if not len(data):
-    #break
-#    s.sendall(data.encode('utf-8'))
-    s.sendto(data.encode('utf-8'), server_address)
-    time.sleep(1)
+    if flag == True:
+    	s.sendto(data, server_address)
+    time.sleep(0.25)
 
 s.shutdown(1)
