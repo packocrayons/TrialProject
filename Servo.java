3bac36aaa7aa0105579aeed82c6c5cf94cd36c5a
packocrayons <gibson.brydon@gmail.com>
2017-02-06 12:01:19 -0500

    Added comments

--- Servo.java
+++ Servo.java
@@ -1,3 +1,24 @@
+//Author : Brydon Gibson (with some help from the internet)
+/*This is a class to control a servo on a raspberry pi 3. It should work on a rpi2 but it is untested.
+Connect the signal pin (the white/light brown wire) to the GPIO pin sent to the constructor of this class. Pinouts for the RPi can be found online.
+Make sure to power the servo appropriately, most don't have reverso polarity protection and most cannot handle more than 5 volts.
+
+A servo works by reading the length of a rectangular pulse, at 50Hz. A 2000us high pulse moves the servo all the way one way, and a 1000us pulse moves it all the way the other way.
+Due to rounding (PULSE_RATIO), the servo does not actually get a 2000us pulse at 180*. Most servos don't actually move a full 180* either.
+You can send most servos down to 900us and up to about 2100us, and they might move a little further.
+If the servo is shaking a lot, please shut it off, they have no temperature protection and might melt themselves.
+*/
+
+/*Note : this class was intended to be run in its own thread, and should be started as such. Example:
+Servo myServo = new Servo("24");
+new Thread(myServo).start();
+myServo.increment_angle();
+... etc
+
+Be careful when adding things to this class, long prints or long operations on this thread (in the functions) will cause the servo to shake and be inaccurate (see PWM jitter).
+It is best to leave this thread as is and do most operations on another thread.
+*/
+
 import java.io.FileWriter;
 import java.io.File;
 
@@ -18,7 +39,7 @@ class Servo implements Runnable {
 	private String channel;
 	private int pulseWidth; //in microseconds, the high time of the pulse
 
-	public Servo(String gPIOChannel){
+	public Servo(String gPIOChannel){ //constructor
 		this(gPIOChannel, 0);
 	}
 
@@ -30,7 +51,7 @@ class Servo implements Runnable {
 		} catch (IOException e){
 			System.err.println("Can't open unexport file : " + e);
 		}
-		FileWriter exportFile = null;
+		FileWriter exportFile = null;									//setup stuff, most of this doesn't need to be touched
 		try{
 			exportFile= new FileWriter("/sys/class/gpio/export");	
 		} catch (IOException e){
@@ -61,7 +82,7 @@ class Servo implements Runnable {
 		} catch (IOException e){
 			System.out.println("Error opening commandChannel : " + e);
 		}
-		while(true){
+		while(true){ //this is the main loop that controls the servo.
 			pulseWidth = (angle*PULSE_RATIO) + 1000;
 			tryWriteAndFlush(commandChannel, GPIO_HIGH);
 			t = System.nanoTime();
@@ -78,7 +99,7 @@ class Servo implements Runnable {
 	}
 
 	public synchronized void set_angle(int newAngle){
-		if (angle < 180 && angle > 0) angle = newAngle;
+		if (newAngle < 180 && newAngle > 0) angle = newAngle;
 	}
 
 	public synchronized void inc_angle(){
